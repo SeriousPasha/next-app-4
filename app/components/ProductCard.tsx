4b7c86c7ea2675e5d4e244e28544709e24f4fcd2
PavelOfficial <pavelofficial@ya.ru>
2025-12-04 19:10:07 +0300

    init commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <h2>Product card</h2>
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
