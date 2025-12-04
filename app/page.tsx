b9459b7c3548ae061708c06b7982f0e00b5870ea
PavelOfficial <pavelofficial@ya.ru>
2025-12-04 21:05:13 +0300

    fetching users

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard'
 
 export default function Home() {
+  
   return (
     <main>
       <h1>Hello world!</h1>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+  id: number
+  name: string
+}
+
+const  UsersPage = async () => {
+  const users: User[] = await fetch('https://jsonplaceholder.typicode.com/users/', {
+    cache: 'no-store'
+  }).then(res => res.json());
+
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <div>{new Date().toLocaleTimeString()}</div>
+      <ul>
+        {users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
